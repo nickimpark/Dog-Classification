7a2938a6f797401297bb6cea4b7bd4d5e0ad31ce
Nikita Parkhomenko <48395531+nickimpark@users.noreply.github.com>
2022-07-27 13:10:06 +0300

    Add files via upload

--- model_eval.py
+++ model_eval.py
@@ -1,10 +1,23 @@
 from config import DATA_PATH, TRAIN_PATH, VAL_PATH, SAVE_PATH
 
 import torch
+import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
 from efficientnet_pytorch import EfficientNet
 import os
+
+idx_to_class = {0: 'Australian terrier',
+                1: 'Beagle',
+                2: 'Border terrier',
+                3: 'Dingo',
+                4: 'English foxhound',
+                5: 'Golden retriever',
+                6: 'Old English sheepdog',
+                7: 'Rhodesian ridgeback',
+                8: 'Samoyed',
+                9: 'Shih-Tzu'
+                }
 
 val_transform = transforms.Compose([
         transforms.Resize(224),
@@ -30,15 +43,29 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 def evaluate_model(model):
     model.eval()   # Set model to evaluate mode
     running_corrects = 0
+    tp = [0 for i in range(9)]
+    tn = [0 for i in range(9)]
+    fp = [0 for i in range(9)]
+    fn = [0 for i in range(9)]
+    precision = [0 for i in range(9)]
     for inputs, labels in val_dataloader:
         inputs = inputs.to(device)
         labels = labels.to(device)
         outputs = model(inputs)
         _, preds = torch.max(outputs, 1)
         running_corrects += torch.sum(preds == labels.data)
+        for i in range(9):
+            tp[i] += np.sum([pred == label.data and label.data == i for pred, label in zip(preds.cpu(), labels.data.cpu())])
+            tn[i] += np.sum([pred == label.data and label.data != i for pred, label in zip(preds.cpu(), labels.data.cpu())])
+            fp[i] += np.sum([pred != label.data and label.data == i for pred, label in zip(preds.cpu(), labels.data.cpu())])
+            fn[i] += np.sum([pred != label.data and label.data != i for pred, label in zip(preds.cpu(), labels.data.cpu())])
 
     acc = running_corrects.double() / data_size
+    for i in range(9):
+        precision[i] = tp[i] / (tp[i] + fp[i])
     print(f'Validation accuracy: {100 * acc:.2f}%')
+    for i in range(9):
+        print(f'Precision for class {idx_to_class[i]}: {100 * precision[i]:.2f}%')
 
 
 model = EfficientNet.from_name('efficientnet-b0', num_classes=num_classes)
